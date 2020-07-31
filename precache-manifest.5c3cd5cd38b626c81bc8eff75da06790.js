514f05c4363fe2d81196f9bbbd0f5953fffac5c0
Aleksandr Mykhailov <ss.mykhailov@gmail.com>
2020-07-31 12:33:44 +0300

    Updates

---
+++ precache-manifest.5c3cd5cd38b626c81bc8eff75da06790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc4402aab926fcae37ac56594322de7",
+    "url": "/react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "91a90d0f7d073622203f",
+    "url": "/react-hw-02-phonebook/static/css/main.463f579d.chunk.css"
+  },
+  {
+    "revision": "f0d814d93e6433311e75",
+    "url": "/react-hw-02-phonebook/static/js/2.4e5ce48e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-02-phonebook/static/js/2.4e5ce48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a90d0f7d073622203f",
+    "url": "/react-hw-02-phonebook/static/js/main.0fb0d945.chunk.js"
+  },
+  {
+    "revision": "daded22ae70b3ac9df87",
+    "url": "/react-hw-02-phonebook/static/js/runtime-main.3d6672e5.js"
+  }
+]);
